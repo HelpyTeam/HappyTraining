ef023c096727ee256054b71e3abf3e8183940edf
Gyokusho <thulnnse61997@fpt.edu.vn>
2017-09-12 18:00:30 +0700

    [Issue #4] Init login sample project.

---
+++ thu2/play-java-jpa-example-2.6.x/target/scala-2.12/routes/main/controllers/routes.java
@@ -0,0 +1,23 @@
+
+// @GENERATOR:play-routes-compiler
+// @SOURCE:E:/GITHUB/HappyTraining/thu/play-java-jpa-example-2.6.x/conf/routes
+// @DATE:Tue Sep 12 17:00:06 ICT 2017
+
+package controllers;
+
+import router.RoutesPrefix;
+
+public class routes {
+  
+  public static final controllers.ReversePersonController PersonController = new controllers.ReversePersonController(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseUserController UserController = new controllers.ReverseUserController(RoutesPrefix.byNamePrefix());
+
+  public static class javascript {
+    
+    public static final controllers.javascript.ReversePersonController PersonController = new controllers.javascript.ReversePersonController(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseUserController UserController = new controllers.javascript.ReverseUserController(RoutesPrefix.byNamePrefix());
+  }
+
+}
